2719586946e12dd837fd883ab4209f818328ab05
tigeryu8900 <tigeryu8900@gmail.com>
2023-05-31 20:04:41 -0700

    support redirect

--- sso/server.js
+++ sso/server.js
@@ -50,36 +50,46 @@ const vKey = JSON.parse(fs.readFileSync("../verification_key.json").toString());
         BigInt(body.nonce);
       } catch (e) {
         console.error("invalid public signals");
-        res.redirect(400, "/register?message=Invalid+public+signals.");
+        res.redirect(400, "/register?message=Invalid+public+signals."
+            + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+                : ""));
         await connection.rollback();
         return;
       }
       if (!session.nonce) {
         console.error("nonce not generated");
-        res.redirect(500, "/register?message=Nonce+not+generated.");
+        res.redirect(500, "/register?message=Nonce+not+generated."
+            + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+                : ""));
         await connection.rollback();
         return;
       }
       if (BigInt(body.nonce) !== BigInt(session.nonce)) {
         console.error("nonce mismatch");
-        res.redirect(400, "/register?message=Nonce+mismatch.");
+        res.redirect(400, "/register?message=Nonce+mismatch."
+            + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+                : ""));
         await connection.rollback();
         return;
       }
       if (await utils.getUser(body.username, connection)) {
         console.error("user already exists");
-        res.redirect(400, "/register?message=User+already+exists.");
+        res.redirect(400, "/register?message=User+already+exists."
+            + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+                : ""));
         await connection.rollback();
         return;
       }
       await utils.addUser(body.username, body.output, body.nonce, connection);
       session.username = body.username;
       console.log("added user", body.username);
-      res.redirect("/");
+      res.redirect(body.redirect ? body.redirect : "/");
       await connection.commit();
     } catch (e) {
       console.error(e);
-      res.redirect(500, "/register?message=Something+went+wrong.");
+      res.redirect(500, "/register?message=Something+went+wrong."
+          + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+              : ""));
       await connection.rollback();
     } finally {
       // await connection.query("UNLOCK TABLES");
@@ -109,42 +119,54 @@ const vKey = JSON.parse(fs.readFileSync("../verification_key.json").toString());
         BigInt(body.nonce);
       } catch (e) {
         console.error("invalid public signals");
-        res.redirect(400, "/signin?message=Invalid+public+signals.");
+        res.redirect(400, "/signin?message=Invalid+public+signals."
+            + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+                : ""));
         await connection.rollback();
         return;
       }
       if (!session.nonce) {
         console.error("nonce not generated");
-        res.redirect(500, "/signin?message=Nonce+not+generated.");
+        res.redirect(500, "/signin?message=Nonce+not+generated."
+            + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+                : ""));
         await connection.rollback();
         return;
       }
       if (BigInt(body.nonce) !== BigInt(session.nonce)) {
         console.error("nonce mismatch");
-        res.redirect(400, "/signin?message=Nonce+mismatch.");
+        res.redirect(400, "/signin?message=Nonce+mismatch."
+            + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+                : ""));
         await connection.rollback();
         return;
       }
       let user = await utils.getUser(body.username, connection);
       if (!user) {
-        res.redirect(400, "/signin?message=User+not+found.");
+        res.redirect(400, "/signin?message=User+not+found."
+            + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+                : ""));
         return;
       }
       if (!await snarkjs.plonk.verify(vKey, [
           user.output,
           user.nonce
       ], JSON.parse(body.proof))) {
-        res.redirect(400, "/signin?message=Username+or+password+is+incorrect.");
+        res.redirect(400, "/signin?message=Username+or+password+is+incorrect."
+            + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+                : ""));
         return;
       }
       await utils.updateUser(body.user, body.output, body.nonce, connection);
       session.username = body.username;
       console.log("user", body.username, "updated");
-      res.redirect("/");
+      res.redirect(body.redirect ? body.redirect : "/");
       await connection.commit();
     } catch (e) {
       console.error(e);
-      res.redirect(500, "/signin?message=Something+went+wrong.");
+      res.redirect(500, "/signin?message=Something+went+wrong."
+          + (body.redirect ? `&redirect=${encodeURIComponent(body.redirect)}`
+              : ""));
       await connection.rollback();
     } finally {
       session.nonce = utils.generateNonce();
